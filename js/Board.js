8f07abd68bddbed73ca4b14e89ab8c9826e983a2
Rodik Hanukaev <rodik@chegg.com>
2015-05-16 20:20:14 +0300

    new board class

--- js/Board.js
+++ js/Board.js
@@ -3,11 +3,221 @@
  */
 (function (window) {
     const EMPTY_TILE = 0;
+    const BREAK = 'break';
+    const STOP = 'stop';
 
-
-    var Board = function () {
-
+    var Board = function (size) {
+        this.size = size || 4;
+        this.reset();
     };
+
+    Board.prototype.reset = function () {
+        var i, j, size = this.size;
+        this.rows = [];
+        this.empty = [];
+        this.board = [];
+        this.highest = 0;
+        this.isMapped = false;
+        for (i = 0; i < size; i++) {
+            this.board[i] = [];
+            this.rows[i] = [];
+            for (j = 0; j < size; j++) {
+                this.board[i][j] = 0;
+            }
+        }
+
+        this.spawnTile();
+        this.spawnTile();
+    };
+
+    Board.prototype.move = function (direction) {
+        var reverse = (direction === 'down' || direction === 'right');
+        var vertical = (direction === 'up' || direction === 'down');
+
+        var moved = false;
+
+        this.each(function (i,j,value) {
+            if (value !== EMPTY_TILE) {
+                this.iterateBack(vertical, reverse, i, j, function (k,l,runner,start,end,previous) {
+                    if (this.board[k][l] !== EMPTY_TILE) {
+                        if (value === this.board[k][l]) {
+                            this.moveTile(i, j, k, l);
+                            moved = true;
+                        } else {
+                            if (runner !== start) {
+                                self.moveTile(i, j, previous[0], previous[1]);
+                                moved = true;
+                            }
+                        }
+                        return BREAK;
+                    } else if (runner === end) {
+                        this.moveTile(i, j, k, l);
+                        moved = true;
+                    }
+                });
+            }
+        }, reverse);
+
+        this.upgradeTiles();
+        this.spawnTile();
+
+        return moved;
+    };
+
+    Board.prototype.moveTile = function (x1,y1,x2,y2) {
+        if (this.board[x1][y1] === this.board[x2][y2]) {
+            this.setTile(x2,y2, this.board[x2][y2] * -2);
+        } else {
+            this.setTile(x2,y2,this.board[x1][x2]);
+        }
+        this.setTile(x1,y1,EMPTY_TILE);
+
+        return true;
+    };
+
+    function compare (a, op, b) {
+        switch (op) {
+            case '>=':  return a >= b;
+            case '<=':  return a <= b;
+        }
+    }
+
+    Board.prototype.iterateBack = function (vertical, reverse, i, j, cb) {
+        var k,
+            op = (reverse) ? '>=' : '<=',
+            end = (reverse) ? this.size - 1 : 0,
+            start = (vertical) ? j : i,
+            inc = (reverse) ? -1 : 1;
+
+        if (reverse) {
+            start--;
+        } else {
+            start++;
+        }
+
+        for (k = start; compare(k,op,end); k = k + inc) {
+            if (vertical) {
+                if (cb.call(this,k,j,k,start,end, [k-inc,j]) === BREAK) {
+                    break;
+                }
+            } else {
+                if (cb.call(this,i,k,k,start,end, [i,k-inc]) === BREAK) {
+                    break;
+                }
+            }
+        }
+    };
+
+    Board.prototype.getAvailableMoves = function () {
+        var moves = [];
+        var boardCopy = this.clone();
+        if (boardCopy.move('left')) {
+            moves.push(this.move.bind(this, 'left'));
+            moves[moves.length - 1].fnName = 'left';
+        }
+        if (boardCopy.move('down')) {
+            moves.push(this.move.bind(this, 'down'));
+            moves[moves.length - 1].fnName = 'down';
+        }
+        if (boardCopy.move('up')) {
+            moves.push(this.move.bind(this, 'up`'));
+            moves[moves.length - 1].fnName = 'up';
+        }
+        if (boardCopy.move('right')) {
+            moves.push(this.move.bind(this, 'right'));
+            moves[moves.length - 1].fnName = 'right';
+        }
+
+        return moves.length ? moves : false;
+    };
+
+    Board.prototype.map = function () {
+        this.each(function (i, j, value) {
+            this.rows[j][i] = value;
+            if (value === EMPTY_TILE) {
+                this.empty.push([i, j]);
+            }
+            if (value > this.highest) {
+                this.highest = value;
+            }
+
+        });
+        this.isMapped = true;
+    };
+
+    Board.prototype.each = function (cb, reverse) {
+        var i, j, size = this.size;
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                if (reverse) {  // right,down
+                    if (cb.call(this, size - 1 - i, size - 1 - j, this.board[i][j]) === STOP) {
+                        return;
+                    }
+                } else {        // left,up
+                    if (cb.call(this, i, j, this.board[i][j]) === STOP) {
+                        return;
+                    }
+                }
+            }
+        }
+    };
+
+    Board.prototype.getRandomEmptyTile = function () {
+        if (!this.isMapped) {
+            this.map();
+        }
+
+        return this.empty ? this.empty[Math.floor(Math.random() * this.empty.length)] : false;
+    };
+
+
+    Board.prototype.setTile = function (i,j,value) {
+        this.rows[j][i] = value;
+        if (this.highest < value) {
+            this.highest = value;
+        }
+        if (this.board[i][j] === EMPTY_TILE) {
+            //todo better way
+            for (var k = 0; k < this.empty.length; k++) {
+                if (this.empty[k][0] === i && this.empty[k][1] === j) {
+                    this.empty.splice(k, 1);
+                    break;
+                }
+            }
+        }
+        this.board[i][j] = value;
+
+        if(value === EMPTY_TILE) {
+            this.empty.push([i,j]);
+        }
+    };
+
+    Board.prototype.spawnTile = function () {
+        var xy = this.getRandomEmptyTile();
+        if (!xy) {
+            return false;
+        }
+
+        this.setTile(xy[0], xy[1], Math.random() < 0.9 ? 2 : 4);
+    };
+
+    Board.prototype.clone = function () {
+        var clone = new Board(this.size);
+        for (var i = 0; i < this.size; i++) {
+            clone.board[i] = this.board[i].slice();
+        }
+        return clone;
+    };
+
+    Board.prototype.upgradeTiles = function () {
+        this.each(function (i,j,value) {
+            if (value < 0) {
+                value = -value;
+                this.setTile(i,j, value);
+            }
+        });
+    };
+
 
     window.Board = Board;
 }(window));
--- js/Game.js
+++ js/Game.js
@@ -1,21 +1,16 @@
-(function (window) {
+(function (window, Board) {
     'use strict';
-
-    const EMPTY_TILE = 0;
-    const BREAK = 'break';
 
     /**
      * Game manager
      * @constructor
      */
     var Game = function () {
-        this.size = 4;
+        this.board = new Board();
         this.gameover = false;
         this.win = false;
         this.reset();
-
         this.bindKeyboard();
-
     };
 
     Game.prototype.bindKeyboard = function () {
@@ -45,333 +40,41 @@
      * Reset the game data to a new game state
      */
     Game.prototype.reset = function () {
-        this.board = [];
-        for (var i = 0; i < this.size; i++) {
-            var row = [];
-            for (var j = 0; j < this.size; j++) {
-                row.push(EMPTY_TILE);
-            }
-            this.board.push(row);
-        }
-
-        this.spawnTile();
-        this.spawnTile();
-
+        this.board.reset();
         console.log('Game Reset');
         this.print();
 
     };
 
     Game.prototype.swipe = function (direction, board) {
-        if (this.gameover) {
-            return;
-        }
         if (!board) {
             board = this.board;
         }
-        var moved = this['swipe' + direction.charAt(0).toUpperCase() + direction.slice(1)](board);
-        this.upgradeTiles(board);
+        if (this.gameover) {
+            return;
+        }
+
+        var moved = board.move(direction);
+        console.log('moved', moved);
         if (!moved) {
             return;
         }
 
-        this.spawnTile(board);
-        if (!this.getAvailableMoves(board)) {
-            if (board === this.board) {
-                this.gameover = true;
-            }
-        }
+        //if (!this.board.getAvailableMoves()) {
+        //    this.gameover = true;
+        //}
         this.print();
     };
 
-    Game.prototype.swipeUp = function (board) {
-        var self = this;
-        var moved = false;
-        this.iterateSlide('up', function (i, j, k) {
-            if (board[k][j] !== EMPTY_TILE) {
-                if (board[k][j] === board[i][j]) {
-                    self.moveTile(k, j, i, j, board);
-                    moved = true;
-                } else {
-                    if (k !== i - 1) {
-                        self.moveTile(k + 1, j, i, j, board);
-                        moved = true;
-                    }
-                }
-                return BREAK;
-            } else if (k === 0) {
-                self.moveTile(k, j, i, j, board);
-                moved = true;
-            }
-        }, board);
-
-        return moved;
-    };
-
-    Game.prototype.swipeDown = function (board) {
-        var self = this;
-        var moved = false;
-        this.iterateSlide('down', function (i, j, k) {
-            if (board[k][j] !== EMPTY_TILE) {
-                if (board[k][j] === board[i][j]) {
-                    self.moveTile(k, j, i, j, board);
-                    moved = true;
-                } else {
-                    if (k !== i + 1) {
-                        self.moveTile(k - 1, j, i, j, board);
-                        moved = true;
-                    }
-                }
-                return BREAK;
-            } else if (k === self.size - 1) {
-                self.moveTile(k, j, i, j, board);
-                moved = true;
-            }
-        }, board);
-        return moved;
-    };
-
-    Game.prototype.swipeLeft = function (board) {
-        var self = this;
-        var moved = false;
-        this.iterateSlide('left', function (i, j, k) {
-            if (board[i][k] !== EMPTY_TILE) {
-                if (board[i][k] === board[i][j]) {
-                    self.moveTile(i, k, i, j, board);
-                    moved = true;
-                } else {
-                    if (k !== j - 1) {
-                        self.moveTile(i, k + 1, i, j, board);
-                        moved = true;
-                    }
-                }
-                return BREAK;
-            } else if (k === 0) {
-                self.moveTile(i, k, i, j, board);
-                moved = true;
-            }
-        }, board);
-
-        return moved;
-    };
-
-    Game.prototype.swipeRight = function (board) {
-        var self = this;
-        var moved = false;
-        this.iterateSlide('right', function (i, j, k) {
-            if (board[i][k] !== EMPTY_TILE) {
-                if (board[i][k] === board[i][j]) {
-                    self.moveTile(i, k, i, j, board);
-                    moved = true;
-                } else {
-                    if (k !== j + 1) {
-                        self.moveTile(i, k - 1, i, j, board);
-                        moved = true;
-                    }
-                }
-                return BREAK;
-            } else if (k === board[i].length - 1) {
-                self.moveTile(i, k, i, j, board);
-                moved = true;
-            }
-        }, board);
-
-        return moved;
-    };
-
-    Game.prototype.getRandomEmptyTile = function (board) {
-        if (!board) {
-            board = this.board;
-        }
-        var randOpts = [];
-        for (var i = 0; i < this.size; i++) {
-            for (var j = 0; j < board[i].length; j++) {
-                if (board[i][j] === EMPTY_TILE) {
-                    randOpts.push([i, j]);
-                }
-            }
-        }
-
-        return randOpts.length ? randOpts[Math.floor(Math.random() * randOpts.length)] : false;
-    };
-
-    Game.prototype.getAvailableMoves = function (board) {
-        var moves = [];
-        var boardCopy = this.getBoardClone(board);
-        if (this.swipeLeft(boardCopy)) {
-            moves.push(this.swipe.bind(this, 'left'));
-            moves[moves.length - 1].fnName = 'left';
-        }
-        if (this.swipeDown(boardCopy)) {
-            moves.push(this.swipe.bind(this, 'down'));
-            moves[moves.length - 1].fnName = 'down';
-        }
-        if (this.swipeUp(boardCopy)) {
-            moves.push(this.swipe.bind(this, 'up'));
-            moves[moves.length - 1].fnName = 'up';
-        }
-        if (this.swipeRight(boardCopy)) {
-            moves.push(this.swipe.bind(this, 'right'));
-            moves[moves.length - 1].fnName = 'right';
-        }
-
-        return moves.length ? moves : false;
-    };
-
-    Game.prototype.spawnTile = function (board) {
-        if (!board) {
-            board = this.board;
-        }
-
-        var xy = this.getRandomEmptyTile(board);
-        if (!xy) {
-            return false;
-        }
-
-        board[xy[0]][xy[1]] = Math.random() < 0.9 ? 2 : 4;
-    };
 
     Game.prototype.print = function () {
-        for (var i = 0; i < this.size; i++) {
-            for (var j = 0; j < this.board[i].length; j++) {
-                var el = document.querySelector('table#game tr:nth-child(' + parseInt(i + 1) + ') td:nth-child(' + parseInt(j + 1) + ')');
-                el.innerText = this.board[i][j];
-                el.setAttribute('data-value', this.board[i][j]);
-            }
-        }
-
-    };
-
-    Game.prototype.getBoardClone = function (board) {
-        if (!board) {
-            board = this.board;
-        }
-
-        var clone = [];
-        for (var i = 0; i < this.size; i++) {
-            clone.push(board[i].slice())
-        }
-        return clone;
-    };
-
-    Game.prototype.getRow = function (i, board) {
-        if (!board) {
-            board = this.board;
-        }
-        var row = [];
-        for (var j = 0; j < this.size; j++) {
-            row.push(board[j][i])
-        }
-        return row;
-    };
-
-    Game.prototype.moveTile = function (i, j, k, p, board) {
-        if (!board) {
-            board = this.board;
-        }
-        if (board[i][j] === board[k][p]) {
-            board[i][j] += 1;
-        } else {
-            board[i][j] = board[k][p];
-        }
-        board[k][p] = EMPTY_TILE;
-
-        return true;
-    };
-
-    Game.prototype.upgradeTiles = function (board) {
-        if (!board) {
-            board = this.board;
-        }
-
-        for (var i = 0; i < this.size; i++) {
-            for (var j = 0; j < this.size; j++) {
-                if (board[i][j] % 2 === 1) {
-                    board[i][j] = (board[i][j] - 1) * 2;
-                    if (board[i][j] === 2048) {
-                        if (board === this.board) {
-                            this.gameover = true;
-                            this.win = true;
-                        }
-                    }
-                }
-            }
-        }
-    };
-
-    Game.prototype.each = function (cb) {
-        var i, j, max = this.size;
-        for (i = 0; i < max; i++) {
-            for (j = 0; j < max; j++) {
-                cb(i,j, value);
-            }
-        }
-    };
-
-    Game.prototype.iterateSlide = function (dir, cb, board) {
-        if (!board) {
-            board = this.board;
-        }
-
-        var i, j, k;
-        var max = this.size;
-        switch (dir) {
-            case 'up':
-                for (j = 0; j < max; j++) {
-                    for (i = 0; i < max; i++) {
-                        if (board[i][j] !== EMPTY_TILE) {
-                            for (k = i - 1; k > -1; k--) {
-                                if (cb(i, j, k) === BREAK) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                break;
-            case 'down':
-                for (j = 0; j < max; j++) {
-                    for (i = max - 1; i > -1; i--) {
-                        if (board[i][j] !== EMPTY_TILE) {
-                            for (k = i + 1; k < max; k++) {
-                                if (cb(i, j, k) === BREAK) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                break;
-            case 'left':
-                for (i = 0; i < max; i++) {
-                    for (j = 0; j < max; j++) {
-                        if (board[i][j] !== EMPTY_TILE) {
-                            for (k = j - 1; k > -1; k--) {
-                                if (cb(i, j, k) === BREAK) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                break;
-            case 'right':
-                for (i = 0; i < max; i++) {
-                    for (j = max - 1; j > -1; j--) {
-                        if (board[i][j] !== EMPTY_TILE) {
-                            for (k = j + 1; k < max; k++) {
-                                if (cb(i, j, k) === BREAK) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                break;
-        }
-
+        this.board.each(function (i,j,value) {
+            var el = document.querySelector('table#game tr:nth-child(' + parseInt(i + 1) + ') td:nth-child(' + parseInt(j + 1) + ')');
+            el.innerText = value;
+            el.setAttribute('data-value', value);
+        });
     };
 
     window.Game = Game;
 
-}(window));
+}(window, window.Board));
